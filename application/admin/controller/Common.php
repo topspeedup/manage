22ddde4f039e42324cc959a1ad51a68ed4941be1
topspeedup <491364922@qq.com>
2017-12-04 09:49:21 +0800

    controller

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -3,14 +3,13 @@
  * Created by PhpStorm.
  * User: topspeedup
  * Date: 2017/12/4
- * Time: 9:44
+ * Time: 9:47
  */
 
 namespace app\admin\controller;
+use think\Controller;
 
-class Index extends Common
+class Common extends Controller
 {
-    public function index(){
-        return view();
-    }
+    
 }
--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -3,12 +3,12 @@
  * Created by PhpStorm.
  * User: topspeedup
  * Date: 2017/12/4
- * Time: 9:44
+ * Time: 9:43
  */
 
 namespace app\admin\controller;
 
-class Index extends Common
+class Login extends Common
 {
     public function index(){
         return view();
